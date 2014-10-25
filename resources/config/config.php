9f68b06259b7e312436796e7ee9d62ef8485d7f4
Alexander Trauzzi <atrauzzi@gmail.com>
2014-10-25 14:58:20 -0500

    Added config files.
    Worth noting that L5 will be changing how the config dirs are setup, so I've got what's likely to be both in place here.  Upon release I'll remove the L4 convention.

---
+++ resources/config/config.php
@@ -0,0 +1,70 @@
+<?php return [
+
+	// Global profile settings, applies to all types, always (even when no profile is requested).
+	'all' => [
+
+		/*
+		 * Global default model adapter.
+		 *
+		 * Can be:
+		 * 		- Atrauzzi\DomainTool\Model\Adapter\AutoGetSet
+		 * 		- Atrauzzi\DomainTool\Model\Adapter\GetSet
+		 * 		- Atrauzzi\DomainTool\Laravel\ModelAdapter
+		 * 		- Any other, supplied by another integration library.
+		 *
+		 * This file by default specifies the Eloquent model adapter.  If removed, Domain Tool
+		 * will default to using the AutoGetSet adapter.
+		 *
+		 */
+		'adapter' => 'Atrauzzi\DomainTool\Laravel\ModelAdapter',
+
+	],
+
+	// Default profiles, per type.
+	'default' => [
+
+		/*
+		 * Default Profiles
+		 *
+		 * If several of your profiles contain similar instructions for a specific class, you
+		 * can avoid that duplication here.  Keep in mind, when no profile is requested, default
+		 * does not get applied.
+		 *
+		'Path\To\Class' => [
+		],
+		 */
+
+	],
+
+	// Profiles, per type.
+	'public' => [
+
+		/*
+		 * Sample profile!
+		 *
+		'Path\To\Class' => [
+
+			// When absent/empty, defaults to AutoGetSet adapter with underscore output.
+			'adapter' => 'Path\To\AdapterImpl',
+
+			// Similar to L4's "fillable" array on models, but supports mapping.
+			// When absent/empty, all attributes can be set.
+			'in' => [
+				'id',
+				'firstName',
+				'tele' => 'phone',
+			],
+			// Similar to L4's "visible" array on models, but supports mapping.
+			// When absent/empty, all attributes are displayed.
+			'out' => [
+				'id',
+				'firstName',
+				'phone' => 'tele',
+			],
+
+		],
+		*/
+
+	],
+
+];
