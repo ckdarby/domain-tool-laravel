38abe78cc3f16338810d36f3ccd2421f01d51804
Alexander Trauzzi <atrauzzi@gmail.com>
2014-10-26 19:50:55 -0500

    Trying something..

--- src/Atrauzzi/DomainTool/Laravel/ServiceProvider.php
+++ src/Atrauzzi/DomainTool/Laravel/ServiceProvider.php
@@ -7,14 +7,16 @@
 
 	class ServiceProvider extends Base {
 
+		public function boot() {
+			$this->package('atrauzzi/domain-tool-laravel', 'domain-tool');
+		}
+
 		/**
 		 * Register the service provider.
 		 *
 		 * @return void
 		 */
 		public function register() {
-
-			$this->package('atrauzzi/domain-tool');
 
 			DomainTool::setMaker([$this->app, 'make']);
 			DomainTool::setProfileLoader($this->app->make('Atrauzzi\DomainTool\Laravel\ProfileLoader'));
